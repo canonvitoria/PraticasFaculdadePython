de01327d09dcf42720596ce9ea456fd93dc02402
Vitória Canon <98993613+canonvitoria@users.noreply.github.com>
2024-05-01 10:04:25 -0300

    Delete Aula - 2/parteCinco.py

--- Aula - 2/parteCinco.py
+++
@@ -1,8 +0,0 @@
-# Aula prática
-
-# Exercício - 1
-# Faça um programa que converte de Fahrenheit para graus Celsius. O programa deve ler uum valor em Fahrenheit, converter e escrever o valor correspondete em Celsius. Para realizar a conversão use a fórmula c = 5/9(f-32)
-
-fahrenheit = float(input('Insira a temperatura em Fahrenheit: '))
-celsius = 5/9 * (fahrenheit - 32) #celsius = 5/9 (fahrenheit - 32)
-print('A temperatura em graus Celsius é de: ', celsius, '°')
