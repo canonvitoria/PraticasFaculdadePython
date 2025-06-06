8bb54f99c9dd8b7d73967b4603564eaa2adf4faf
VitoriaCanon <vitoriacanon1212@gmail.com>
2025-06-06 12:39:42 -0300

    Aula-4 Parte 5

---
+++ Aula-4/parteCinco.py
@@ -0,0 +1,25 @@
+# Exercício 1 - Determinar se oano é bissexto ou não
+
+# ano = int(input("Ano: "))
+# if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+#     print("É bissexto!")
+# else:
+#     print("Não é bissexto!")
+
+# Exercício 2- Determinar o total de dias em um mês e ano 
+
+# mes = int(input("Mês: "))
+# ano = int(input("Ano: "))
+
+# if mes == 2:
+#     if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+#         dias = 29
+#     else:
+#         dias = 28
+# elif mes == 4 or mes == 6 or mes == 9 or mes == 11:
+#     dias = 30
+# else:
+#     dias = 31
+# print(f"Total de dias: {dias}")
+
+
